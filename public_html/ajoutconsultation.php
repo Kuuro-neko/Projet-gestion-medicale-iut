3c9c355adca9515b68836863b14a9d7a10b18340
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@mgail.com>
2022-04-07 17:39:10 +0200

    calendrier ok, ajout consultation interface ok

---
+++ public_html/ajoutconsultation.php
@@ -0,0 +1,148 @@
+<?php
+session_start();
+setlocale(LC_ALL, 'fr_FR.utf8','fra');
+require 'php/config.php';
+$thisPage = "ajoutconsultation";
+
+// Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+if(empty($_SESSION['signedin'])) {
+	header("Location: index.php");
+}
+// Déconnexion de l'utilisateur
+if(isset($_GET["disconnect"])) {
+	session_destroy();
+	header("Location: index.php");
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Projet Gestion médicale">
+	<meta name="keywords" content="HTML, CSS, Gestion médicale, IUT Toulouse">
+	<meta name="author" content="Gonzalez Oropeza Gilles">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/ajoutconsultation.css">
+	<link rel="stylesheet" type="text/css" href="css/common.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+
+
+	<link rel="icon" href="images/logo.png" />
+	<title>Projet Gestion médicale</title>
+</head>
+
+<body>
+	<?php
+		include "php/header.php";
+	?>
+	<h1>Ajouter une consultation</h1>
+	
+
+    <?php if(empty($_GET['id_patient'])) { ?>
+        <!-- Selection du patient -->
+        <fieldset>
+            <legend class="title">Choisissez un patient</legend>
+            <form method="get">
+                <div class="textinputs">
+                   <select name="id_patient" required>
+                        <option value="">Choix du patient</option>
+                        <?php
+                            require "php/connexiondb.php";
+                            $req = $linkpdo->prepare('SELECT nom, prenom, id_patient FROM patient ORDER BY nom');
+                            $req->execute();
+                            while($data = $req->fetch()) {
+                                echo "<option value=\"".$data['id_patient']."\">".$data['nom']." ".$data['prenom']."</option>";
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div class="submitinputs">
+                    <input type="submit" value="Valider"></input>
+                    <button class="cancel" onclick="location.href='consultations.php'" type="button">Annuler</button>
+                </div>
+                <?php 
+                if(!empty($_GET['date'])) {
+                    echo "<input type=\"hidden\" name=\"date\" value=\"".$_GET['date']."\"></input>";
+                }
+                ?>
+            </form>
+        </fieldset>
+
+    <?php } else { ?>
+
+        <!-- Selection du créneau et du médecin -->
+        <fieldset>
+            <legend class="title">Choisissez le créneau</legend>
+            <form method="post" action="php/requeteajouterconsultation.php">
+                <div class="textinputs">
+                <?php 
+                if(!empty($_GET['date'])) {
+                    $placeholder = " placeholder=\"".date('Y-m-d', date($_GET['date']))."\"";
+                } else {
+                    $placeholder ="";
+                }
+                ?>
+                    Date <input type="date" name="date"<?php echo $placeholder;?>> required></input>
+                    <div class="heures">
+                        
+                        <fieldset class="time">
+                        <legend class="timelegend">Heure de début</legend>
+                            Heures <input type="number" name="debutheure" value="0" min="0" max="24" required></input>
+                            Minutes <input type="number" name="debutmin" value="0" min="0" max="59" required></input>
+                        </fieldset>
+                        
+                        <fieldset class="time">
+                        <legend class="timelegend">Durée</legend>
+                            Heures <input type="number" name="dureeheure" value="0" min="0" max="10" required></input>
+                            Minutes <input type="number" name="dureemin" value="0" min="0" max="59" required></input>
+                        </fieldset>
+
+                    </div>
+
+                    Médecin
+                    <select name="medecin">
+                        <?php
+                        require 'php/connexiondb.php';
+                        $req = $linkpdo->prepare('SELECT nom, prenom, id_medecin FROM medecin WHERE medecin.id_medecin = (SELECT patient.id_medecin FROM patient WHERE id_patient = :id_patient)');
+                        $req->execute(array('id_patient'=>$_GET['id_patient']));
+                       
+                        if($data = $req->fetch()) {
+                            // Si le patient a un médecin traitent, le proposer
+                            echo "<option value=\"".$data['id_medecin']."\">".$data['nom']." ".$data['prenom']."</option>";
+                            $requ = $linkpdo->prepare("SELECT nom, prenom, id_medecin FROM medecin where id_medecin <> :idmed ORDER BY nom");
+                            $requ->execute(array('idmed'=>$data['id_medecin']));
+                            while($data2 = $requ->fetch()) {
+                                echo "<option value=\"".$data2['id_medecin']."\">".$data2['nom']." ".$data2['prenom']."</option>";
+                            }
+                        } else { ?>
+                            <option value="NULL">Choix du médecin</option>
+                            <?php
+                            // Sinon, montrer tous les médecins
+
+                            $requ = $linkpdo->prepare("SELECT nom, prenom, id_medecin FROM medecin ORDER BY nom");
+                            $requ->execute();
+                            while($data2 = $requ->fetch()) {
+                                echo "<option value=\"".$data2['id_medecin']."\">".$data2['nom']." ".$data2['prenom']."</option>";
+                            }
+                       } ?>
+                    </select>
+
+                </div>
+                <div class="submitinputs">
+                    <input type="submit" name="ajouter" value="Ajouter"></input>
+                    <button class="cancel" onclick="location.href='ajoutconsultation.php'" type="button">Annuler</button>
+                </div>
+
+            </form>
+        </fieldset>
+
+	<?php
+    }
+		include "php/footer.php";
+	?>
+</body>
+</html>
--- public_html/consultations.php
+++ public_html/consultations.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+setlocale(LC_TIME, 'fr_FR.utf8','fra');
 require 'php/config.php';
 $thisPage = "consultations";
 
@@ -12,11 +13,65 @@ if(isset($_GET["disconnect"])) {
 	session_destroy();
 	header("Location: index.php");
 }
-if(empty($_GET['week'])) {
-	$week = intval(date("W", time()));
-} else {
-	$week = $_GET['week'];
+
+
+
+// Retourne le nombre de semaines (int) d'une année (int)
+function getWeeksInYear($year) {
+    $date = new DateTime;
+    $date->setISODate($year, 53);
+    return ($date->format("W") === "53" ? 53 : 52);
 }
+
+// Retourne un tableau contenant les 7 jours d'une semaine d'une année
+function daysInWeek($yearNum, $weekNum)
+{
+    $result = array();
+    $datetime = new DateTime();
+    $datetime->setISODate($yearNum, $weekNum, 1);
+    $interval = new DateInterval('P1D');
+    $week = new DatePeriod($datetime, $interval, 6);
+	
+	
+    foreach($week as $day){
+        $result[] = $day->getTimestamp();
+    }
+    return $result;
+}
+
+
+
+// Affecter les valeurs à l'année et semaine affichée sur la page
+if(empty($_GET['year'])) {
+	$_SESSION['year'] = intval(date("Y", time()));
+} else {
+	$_SESSION['year'] = $_GET['year'];
+}
+if(empty($_GET['week'])) {
+	$_SESSION['week'] = intval(date("W", time()));
+} else {
+	$_SESSION['week'] = $_GET['week'];
+}
+
+// Variables pour la semaine suivante
+if ($_SESSION['week'] + 1 > getWeeksInYear($_SESSION['year'])) {
+	$nextYear = $_SESSION['year'] + 1;
+	$nextWeek = 1;
+} else {
+	$nextYear = $_SESSION['year'];
+	$nextWeek = $_SESSION['week'] + 1;
+}
+
+// Variables pour la semaine précédente
+if ($_SESSION['week'] - 1 < 1) {
+	$prevYear = $_SESSION['year'] - 1;
+	$prevWeek = getWeeksInYear($_SESSION['year'] - 1);
+} else {
+	$prevYear = $_SESSION['year'];
+	$prevWeek = $_SESSION['week'] - 1;
+}
+
+$currentWeek = daysInWeek($_SESSION['year'], $_SESSION['week']);
 
 ?>
 <!DOCTYPE html>
@@ -41,49 +96,36 @@ if(empty($_GET['week'])) {
 		include "php/header.php";
 	?>
 	<h1>Consultations</h1>
-	<p>En développement ! :)</p>
 	<div class="ajouterHorsCalendrier">
-		ajouter consultation hors calendrier
+		<button class="retourSemaineActuelle" onclick="location.href='ajoutconsultation.php'" type="button">Créer une nouvelle consultation</button>
 	</div>
 	<div class="semaines">
-		<button class="semainrePrec" onclick="location.href='consultations.php?week=<?php echo $week - 1; ?>'" type="button">⇦ Semaine <?php echo $week - 1; ?></button>
+		<button class="semainrePrec" onclick="location.href='consultations.php?week=<?php echo $prevWeek.'&year='.$prevYear; ?>'" type="button">⇦ Semaine <?php echo $prevWeek; ?></button>
 		<div id="centreSemaines">
-			<p id="dispSemaine">Semaine <?php echo $week; ?></p>
-			<?php if(!empty($_GET['week'])) {
-				if($_GET['week'] !==  intval(date("W", time()))) {
-			?> <button class="retourSemaineActuelle" onclick="location.href='consultations.php'" type="button">Retour semaine <?php echo intval(date("W", time())); ?></button> <?php
-				}
-			} ?>
+			<p id="dispSemaine">Année <?php echo $_SESSION['year']; ?> Semaine <?php echo $_SESSION['week']; ?></p>
+			<?php
+				if($_SESSION['week'] !==  intval(date("W", time()))) {
+			?> <button class="retourSemaineActuelle" onclick="location.href='consultations.php'" type="button">Retour semaine courante</button> <?php
+				} ?>
 		</div>
-		<button class="semaineSuiv" onclick="location.href='consultations.php?week=<?php echo $week + 1; ?>'" type="button">Semaine <?php echo $week + 1; ?> ⇨</button>
+		<button class="semaineSuiv" onclick="location.href='consultations.php?week=<?php echo $nextWeek.'&year='.$nextYear; ?>'" type="button">Semaine <?php echo $nextWeek; ?> ⇨</button>
 	</div>
+	<?php require "php/connexiondb.php"; ?>
+
 	<div class="consultations">
-		<div class="jour">
-			<p class="nomJour">Lundi</p>
-
-		</div>
-		<div class="jour">
-			<p class="nomJour">Mardi</p>
-
-		</div>
-		<div class="jour">
-			<p class="nomJour">Mercredi</p>
-
-		</div>
-		<div class="jour">
-			<p class="nomJour">Jeudi</p>
-
-		</div>
-		<div class="jour">
-			<p class="nomJour">Vendredi</p>
-
-		</div>
-		<div class="jour">
-			<p class="nomJour">Samedi</p>
-
-		</div>
+		<?php
+		for($i = 0; $i < 6; $i++) {
+			$position = "";
+			if($i == 0) {
+				$position = " jourGauche";
+			} elseif ($i == 5) {
+				$position = " jourDroite";
+			}
+			echo "<div class=\"jour".$position."\"><p class=\"nomJour\">".ucfirst(strftime("%A%e %B", $currentWeek[$i]))."</p></div>";
+		}
+		?>
 	</div>
-	<table>
+
 	<?php
 		include "php/footer.php";
 	?>
